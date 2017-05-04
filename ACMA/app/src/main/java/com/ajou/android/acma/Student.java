2c6113cfc10f71a7f29dc4206f8a9e5a3deb8ab2
jungminKim94 <vencott@naver.com>
2017-05-04 18:51:28 +0900

    project create

---
+++ ACMA/app/src/main/java/com/ajou/android/acma/Student.java
@@ -0,0 +1,19 @@
+package com.ajou.android.acma;
+
+public class Student {
+    private String id;
+    private String password;
+    private String name;
+    private int grade;
+    private int sex;
+
+    public Student(String id, String password, String name, int grade, int sex) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.grade = grade;
+        this.sex = sex;
+    }
+
+
+}
